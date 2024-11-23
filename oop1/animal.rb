76ee1ecd2b1881811ab159f97f188423434a7af8
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-11-24 00:18:55 +0900

    継承を忘れていたので追加

--- oop1/animal.rb
+++ oop1/animal.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
+require 'debug'
 
 class Animal
-  def initialize(name, age)
+  def initialize(name = 'デフォ')
     @name = name
-    @age = age
   end
 
   def eat
@@ -11,15 +11,18 @@ class Animal
   end
 end
 
-class Human
+class Human < Animal
   def eat
-    p "人間の#{name}は食べます"
+    p "人間の#{@name}は食べます"
   end
 end
 
-class Dog
+class Dog < Animal
   def eat
-    p "わん、#{name}は食べるわん"
+    p "わん、#{@name}は食べるわん"
   end
 end
+
+p Human.new.eat
+p Dog.new.eat
 
